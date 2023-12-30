8e600dce520fe1f6701985380f52fdcd1638e861
Hector Diaz <hectordb19@gmail.com>
2023-12-30 01:14:20 -0300

    Agregando pagina jeshu

---
+++ PaginaWeb/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="style.css">
+  <title>Bengu.Art</title>
+</head>
+
+<body>
+  <header>
+    <!--Logo-->
+    <a href="#" target="" rel="noopener noreferrer">
+      <img src="Logo.jpg" alt="Logo" height="80px" width="95px" /></a>
+    <!--Nombre-->
+    <h1>Bengu.Art</h1>
+  </header>
+  <section class="contenido">
+    <?php
+    $directorio = 'Imagenes'; // Ruta de tu carpeta con las imágenes
+    $archivos = scandir($directorio);
+
+    foreach ($archivos as $archivo) {
+      if ($archivo !== '.' && $archivo !== '..') {
+        echo '<img src="' . $directorio . '/' . $archivo . '" alt="Descripción de la imagen" height="200px" width="200px">';
+      }
+    }
+    ?>
+
+  </section>
+  <footer>
+    <ul class="sci">
+      <li><a href="#"><ion-icon name="logo-facebook"></ion-icon></a></li>
+      <li><a href="#"><ion-icon name="logo-instagram"></ion-icon></a></li>
+      <li><a href="#"><ion-icon name="logo-whatsapp"></ion-icon></a></li>
+    </ul>
+  </footer>
+</body>
+<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
+</html>
