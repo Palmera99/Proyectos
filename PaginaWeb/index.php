ec336e4e30ec67e748d1caed22b0a52809589cd3
Palmera99 <hectordb19@gmail.com>
2023-12-31 00:49:04 -0300

    añadiendo cosas

--- PaginaWeb/index.php
+++ PaginaWeb/index.php
@@ -1,23 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="style.css">
-  <title>Bengu.Art</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css">
+    <title>Bengu.Art</title>
 </head>
 
 <body>
-  <header>
-    <!--Logo-->
-    <a href="#" target="" rel="noopener noreferrer">
-      <img src="Logo.jpg" alt="Logo" height="80px" width="95px" /></a>
-    <!--Nombre-->
-    <h1>Bengu.Art</h1>
-  </header>
-  <section class="contenido">
-    <?php
+    <header>
+        <!--Logo-->
+        <a href="#" target="" rel="noopener noreferrer">
+            <img src="Logo.jpg" alt="Logo" height="80px" width="95px" /></a>
+        <!--Nombre-->
+        <a href="#" target="" rel="noopener noreferrer">
+            <h1>Bengu.Art</h1>
+        </a>
+    </header>
+    <section class="contenido">
+        <?php
     $directorio = 'Imagenes'; // Ruta de tu carpeta con las imágenes
     $archivos = scandir($directorio);
 
@@ -28,14 +30,20 @@
     }
     ?>
 
-  </section>
-  <footer>
-    <ul class="sci">
-      <li><a href="#"><ion-icon name="logo-facebook"></ion-icon></a></li>
-      <li><a href="#"><ion-icon name="logo-instagram"></ion-icon></a></li>
-      <li><a href="#"><ion-icon name="logo-whatsapp"></ion-icon></a></li>
-    </ul>
-  </footer>
+    </section>
+    <footer>
+        <ul class="sci">
+            <li><a href="#">
+                    <ion-icon name="logo-facebook"></ion-icon>
+                </a></li>
+            <li><a href="#">
+                    <ion-icon name="logo-instagram"></ion-icon>
+                </a></li>
+            <li><a href="#">
+                    <ion-icon name="logo-whatsapp"></ion-icon>
+                </a></li>
+        </ul>
+    </footer>
 </body>
 <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
